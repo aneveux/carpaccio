d8c860e45fe3aa611fb778d62a5918c351ac86f4
Antoine Neveux <aneveux@protonmail.com>
2017-12-11 23:54:52 +0100

    first import - now having a ready to code project :)

---
+++ src/main/kotlin/com/github/aneveux/carpaccio/Carpaccio.kt
@@ -0,0 +1,5 @@
+package com.github.aneveux.carpaccio
+
+fun main(args: Array<String>) {
+    println("Hello Carpaccio!")
+}
---
+++ src/test/kotlin/com/github/aneveux/carpaccio/TestCarpaccio.kt
@@ -0,0 +1,13 @@
+package com.github.aneveux.carpaccio
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class TestCarpaccio {
+
+    @Test
+    fun testHello() {
+        assertThat(true).isTrue()
+    }
+
+}
